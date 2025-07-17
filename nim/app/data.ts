482deff034802eef69db3760cbec997285bbecdd
Porter-smith <portersmith@ymail.com>
2025-07-17 17:28:53 -0600

    add nim

---
+++ nim/app/data.ts
@@ -0,0 +1,95 @@
+type Project = {
+  name: string
+  description: string
+  link: string
+  video: string
+  id: string
+}
+
+type WorkExperience = {
+  company: string
+  title: string
+  start: string
+  end: string
+  link: string
+  id: string
+}
+
+type BlogPost = {
+  date: string
+  title: string
+  description: string
+  link: string
+  uid: string
+}
+
+type SocialLink = {
+  label: string
+  link: string
+}
+
+export const PROJECTS: Project[] = [
+  {
+    name: 'Motion Primitives Pro',
+    description:
+      'Advanced components and templates to craft beautiful websites.',
+    link: 'https://pro.motion-primitives.com/',
+    video:
+      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+    id: 'project1',
+  },
+  {
+    name: 'Motion Primitives',
+    description: 'UI kit to make beautiful, animated interfaces.',
+    link: 'https://motion-primitives.com/',
+    video:
+      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+    id: 'project2',
+  },
+]
+
+export const WORK_EXPERIENCE: WorkExperience[] = [
+  {
+    title: 'Software Engineer',
+    company: 'Velosimo',
+    start: '2022',
+    end: '2023',
+    link: 'https://velosimo.com',
+    id: 'work1',
+  },
+  {
+    company: 'Freelance',
+    title: 'Fullstack Engineer',
+    start: '2023',
+    end: '2024',
+    link: 'https://radishapps.com',
+    id: 'work2',
+  },
+  {
+    company: 'Velosimo',
+    title: 'Software Engineer',
+    start: '2025',
+    end: 'Present',
+    link: 'https://velosimo.com',
+    id: 'work3',
+  },
+]
+
+export const BLOG_POSTS: BlogPost[] = [
+        {
+          title: 'I Tried Every Text Editor So You Don\'t Have To',
+          description: 'A comprehensive review of 50+ text editors in 2025',
+          link: '/blog/testing-every-text-editor',
+          uid: 'blog-1',
+    date: '2025-01-15',
+        }
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    label: 'Github',
+    link: 'https://github.com/porter-smith',
+  }
+]
+
+export const EMAIL = 'contact@portersmith.dev'
